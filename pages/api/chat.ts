924e598c856d2cb7fbec54e0ae21eaf51978db42
Felix <30859645+FelixMildon@users.noreply.github.com>
2023-01-22 20:13:19 +1300

    Tweaked againnnn

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -46,7 +46,7 @@ const generatePromptFromMessages = (messages: Message[]) => {
 export default async function handler(req: any, res: any) {
   const messages = req.body.messages
   const messagesPrompt = generatePromptFromMessages(messages)
-  const defaultPrompt = `Doctor seeing a patient in person. Ask questions to make a medical diagnosis. Refrain from recommending patients to seek additional medical attention instead ask more questions. \n\nThis is the conversation between a doctor and a sick person.\n\n${botName}: ${firstMessge}\n${userName}: ${messagesPrompt}\n${botName}: `
+  const defaultPrompt = `Doctor seeing a patient in person. \n\nThis is the conversation between a doctor and a sick person.\n\n${botName}: ${firstMessge}\n${userName}: ${messagesPrompt}\n${botName}: `
   const finalPrompt = process.env.AI_PROMPT
     ? `${process.env.AI_PROMPT}${messagesPrompt}\n${botName}: `
     : defaultPrompt
